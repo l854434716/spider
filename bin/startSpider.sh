cc8900baa32e9a9d26149f724c1ababfe07bf38b
luozhi <l854434716@163.com>
2018-06-07 22:17:25 +0800

    add timezone set

--- bin/startSpider.sh
+++ bin/startSpider.sh
@@ -5,6 +5,6 @@
 . ./setEnv.sh
 export CLASSPATH=$CLASSPATH
 base_dir=$(dirname $0)
-$JAVA_HOME/bin/java -Xms256m -Xmx1024m  -Dbase.dir=$base_dir/../ -Dlogback.configurationFile=$base_dir/../conf/logback.xml  -Dfile.encoding=UTF-8 -Dspider.home=`pwd` "manke.spider.processor.bibi.BibiAnimeIndexPageProcessor"
-$JAVA_HOME/bin/java -Xms256m -Xmx1024m  -Dbase.dir=$base_dir/../ -Dlogback.configurationFile=$base_dir/../conf/logback.xml  -Dfile.encoding=UTF-8 -Dspider.home=`pwd` "manke.spider.processor.qq.QqAnimeIndexPageProcessor"
-$JAVA_HOME/bin/java -Xms256m -Xmx1024m  -Dbase.dir=$base_dir/../ -Dlogback.configurationFile=$base_dir/../conf/logback.xml  -Dfile.encoding=UTF-8 -Dspider.home=`pwd` "manke.spider.processor.youku.YoukuAnimeIndexPageProcessor"
+$JAVA_HOME/bin/java -Xms256m -Xmx1024m -Duser.timezone=GMT+08 -Dbase.dir=$base_dir/../ -Dlogback.configurationFile=$base_dir/../conf/logback.xml  -Dfile.encoding=UTF-8 -Dspider.home=`pwd` "manke.spider.processor.bibi.BibiAnimeIndexPageProcessor"
+$JAVA_HOME/bin/java -Xms256m -Xmx1024m -Duser.timezone=GMT+08 -Dbase.dir=$base_dir/../ -Dlogback.configurationFile=$base_dir/../conf/logback.xml  -Dfile.encoding=UTF-8 -Dspider.home=`pwd` "manke.spider.processor.qq.QqAnimeIndexPageProcessor"
+$JAVA_HOME/bin/java -Xms256m -Xmx1024m -Duser.timezone=GMT+08 -Dbase.dir=$base_dir/../ -Dlogback.configurationFile=$base_dir/../conf/logback.xml  -Dfile.encoding=UTF-8 -Dspider.home=`pwd` "manke.spider.processor.youku.YoukuAnimeIndexPageProcessor"
