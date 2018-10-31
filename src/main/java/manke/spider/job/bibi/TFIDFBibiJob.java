596fab5755d34481b59035b7a10cde6980540cfd
luozhi <l854434716@163.com>
2018-10-31 20:44:54 +0800

    bugs fix

--- src/main/java/manke/spider/job/bibi/TFIDFBibiJob.java
+++ src/main/java/manke/spider/job/bibi/TFIDFBibiJob.java
@@ -5,6 +5,7 @@ import manke.spider.job.AbstractJob;
 import org.bson.Document;
 
 public class TFIDFBibiJob extends AbstractJob<FindIterable<Document>,String> {
+
     @Override
     public void etl() {
 
