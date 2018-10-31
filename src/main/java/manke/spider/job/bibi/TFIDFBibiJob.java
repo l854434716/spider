1949874e5429364b986509602a1c5023c7d6b48a
luozhi <l854434716@163.com>
2018-10-31 20:43:23 +0800

    add  bibi  season  info etl job  for  TF-IDF

---
+++ src/main/java/manke/spider/job/bibi/TFIDFBibiJob.java
@@ -0,0 +1,12 @@
+package manke.spider.job.bibi;
+
+import com.mongodb.client.FindIterable;
+import manke.spider.job.AbstractJob;
+import org.bson.Document;
+
+public class TFIDFBibiJob extends AbstractJob<FindIterable<Document>,String> {
+    @Override
+    public void etl() {
+
+    }
+}
